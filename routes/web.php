82a0c5fd3ab3cc5f9db8478823d7352a0ba08f18
mhdkevn09-oss <mhdkevn09@gmail.com>
2026-05-22 00:41:50 +0700

    Add patient and payment dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/patients', [PatientController::class, 'index']);
+
+Route::get('/payments', [PaymentController::class, 'index']);
+
+Route::post('/payments/pay/{id}', [PaymentController::class, 'pay']);
