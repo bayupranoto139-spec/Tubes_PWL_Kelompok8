eba9c65d38fcd8087dd1aa23a63db4c7fdea4c02
bayu <bayupranoto139@gmail.com>
2026-06-02 14:52:22 +0700

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MidtransController;
+use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/auth.php';
-use App\Http\Controllers\PatientController;
+use App\Http\Controllers\Patient\PatientPanelController;
 use App\Http\Controllers\PaymentController;
+use App\Models\Bill;
 
-
-Route::get('/patients', [PatientController::class, 'index']);
-Route::get('/patients/create', [PatientController::class, 'create']);
-Route::post('/patients/store', [PatientController::class, 'store']);
-
+// NOTE: routes below are for the patient portal (PatientPanelController).
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/payment/{bill}', [MidtransController::class, 'createPayment'])->name('payment.create');
@@ -26,10 +22,10 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::get('/payment-test/{id}', function ($id) {
-    $bill = \App\Models\Bill::find($id) ?? \App\Models\Bill::factory()->create();
-    return app(\App\Http\Controllers\MidtransController::class)->createPayment($bill);
+    $bill = Bill::find($id) ?? Bill::factory()->create();
+
+    return app(MidtransController::class)->createPayment($bill);
 });
-Route::get('/patients', [PatientController::class, 'index']);
 
 Route::get('/payments', [PaymentController::class, 'index']);
 
@@ -37,6 +33,7 @@ Route::post('/payments/pay/{id}', [PaymentController::class, 'pay']);
 
 // Patient Panel Routes
 use App\Http\Controllers\Patient\PatientPanelController;
+use App\Models\Bill;
 
 Route::middleware(['auth'])->prefix('user/patient')->name('patient.')->group(function () {
     Route::get('/dashboard', [PatientPanelController::class, 'dashboard'])->name('dashboard');
