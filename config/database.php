4315d78b2c727055e4077a030ce6dea8777c0960
bayu <bayupranoto139@gmail.com>
2026-06-10 22:21:13 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -52,18 +52,13 @@ return [
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
 
-            'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'sslmode' => 'required',
-        ],
 
-        'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        ]) : [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ]) : [],
+        ],
 
         'mariadb' => [
             'driver' => 'mariadb',
